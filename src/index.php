f1b28db744dbebbd0f68a3e3412a2f3f24c7c47e
Romain DURAND <146010665+Sonyoss@users.noreply.github.com>
2026-03-03 10:04:47 +0100

    fix: CSS importé depuis un fichier style

--- src/index.php
+++ src/index.php
@@ -4,17 +4,18 @@
 <head>
     <meta charset="UTF-8">
     <title>R6.06 Maintenance applicative</title>
+    <link rel="stylesheet" href="styles/style.css">
 </head>
 
 <body>
     <header>
         <h1>R6.06 Maintenance applicative</h1>
-        <h2 style="color: crimson">Evaluation</h2>
-        <p style="color: crimson">Modifiez ce projet à l'aide des outils vus ensemble pour améliorer la maintenabilité
+        <h2 class="red">Evaluation</h2>
+        <p class="red">Modifiez ce projet à l'aide des outils vus ensemble pour améliorer la maintenabilité
             de ce projet et déployez le sur le serveur mis à votre disposition</p>
-        <p style="color: crimson">Vous êtes libre de modifier ce que vous souhaitez sur le projet, chaque amélioration
+        <p class="red">Vous êtes libre de modifier ce que vous souhaitez sur le projet, chaque amélioration
             (ou début d'amélioration) sera prise en compte dans la notation</p>
-        <p style="color: crimson; font-weight: bold; border: solid 2px crimson; padding: 5px; width: fit-content;">
+        <p class="red cadre">
             Pensez à inviter cdiiv sur votre projet Github</p>
     </header>
 
@@ -59,16 +60,16 @@
     <table>
         <thead style="font-weight: bold;">
             <tr>
-                <td style="border: solid black 1px">Id</td>
-                <td style="border: solid black 1px">Text</td>
+                <td class="tabCell">Id</td>
+                <td class="tabCell">Text</td>
             </tr>
         </thead>
         <tbody>
             <?php
             foreach ($d as $val) { ?>
                 <tr>
-                    <td style="border: solid black 1px"><?= $val['id'] ?></td>
-                    <td style="border: solid black 1px"><?= $val['text'] ?></td>
+                    <td class="tabCell"><?= $val['id'] ?></td>
+                    <td class="tabCell"><?= $val['text'] ?></td>
                 </tr>
                 <?php
             } ?>
